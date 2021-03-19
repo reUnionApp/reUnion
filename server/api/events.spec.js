3b85a4c4809063bed3db8400a18387fe7e8fbdd3
Sung Lee <sungyonglee414@gmail.com>
2021-03-19 13:20:28 -0700

    fix: events spec bug fixed

--- server/api/events.spec.js
+++ server/api/events.spec.js
@@ -1,6 +1,7 @@
 const request = require("supertest");
 const app = require("../../index");
 const db = require("../db");
+const { Event } = require("../db/models");
 
 // getting tomorrow's date
 const tomorrow = new Date();
@@ -11,7 +12,19 @@ describe("Event API", () => {
     db.sync();
   });
 
+  afterAll(async (done) => {
+    db.close(done);
+  });
+  
   describe("Create Event Route", () => {
+    afterEach(async () => {
+      const event = Event.destroy({
+        where: {
+          eventName: "Farhads Free Fajitas Party",
+        },
+      });
+    });
+
     it("should create a new event", async () => {
       const res = await request(app)
         .post("/api/events")
@@ -46,16 +59,64 @@ describe("Event API", () => {
   });
 
   describe("Delete Single Event Route", () => {
+    beforeEach(async () => {
+      const event = await Event.create({
+        eventName: "Red Sweater Reunion",
+        eventType: ["class reunion"],
+        owner: "Sung Lee",
+        coordinator: ["Sung Lee"],
+        description: "Reunion for wearing red sweater.",
+        location: "online",
+        startDate: Date.now(),
+        endDate: tomorrow,
+        startTime: "07:00 AM",
+        endTime: "05:00 PM",
+      });
+    });
+
     it("should delete a single event", async () => {
-      const res = await request(app).delete("/api/events/2");
+      const testEvent = await Event.findOne({
+        where: {
+          eventName: "Red Sweater Reunion",
+        },
+      });
+      const res = await request(app).delete(`/api/events/${testEvent.id}`);
       expect(res.status).toEqual(200);
     });
   });
 
   describe("Update Event Route", () => {
+    beforeEach(async () => {
+      const event = await Event.create({
+        eventName: "Red Sweater Reunion",
+        eventType: ["class reunion"],
+        owner: "Sung Lee",
+        coordinator: ["Sung Lee"],
+        description: "Reunion for wearing red sweater.",
+        location: "online",
+        startDate: Date.now(),
+        endDate: tomorrow,
+        startTime: "07:00 AM",
+        endTime: "05:00 PM",
+      });
+    });
+
+    afterEach(async () => {
+      const event = Event.destroy({
+        where: {
+          eventName: "Gretas Green Berets Reunion",
+        },
+      });
+    });
+
     it("should update a single event", async () => {
+      const testEvent = await Event.findOne({
+        where: {
+          eventName: "Red Sweater Reunion",
+        },
+      });
       const res = await request(app)
-        .put("/api/events/1")
+        .put(`/api/events/${testEvent.id}`)
         .send({
           eventName: "Gretas Green Berets Reunion",
           eventType: ["class reunion"],
@@ -70,9 +131,5 @@ describe("Event API", () => {
         });
       expect(res.status).toEqual(200);
     });
-  });
-
-  afterAll(async (done) => {
-    db.close(done);
   });
 });
