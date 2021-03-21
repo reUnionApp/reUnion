5f6b9f7212eb27d98d8183e077e19bab1efe5e2e
Svetlana Leonova <leonova_svetlana@yahoo.com>
2021-03-21 11:33:52 -0400

    test: added hooks to activites specs, fix: fixed a typo in users specs

--- server/api/activities.spec.js
+++ server/api/activities.spec.js
@@ -1,66 +1,119 @@
-const request = require('supertest');
-const app = require('../../index');
-const db = require('../db');
+const request = require("supertest");
+const app = require("../../index");
+const db = require("../db");
+const { Activity } = require("../db/models");
 
 // getting tomorrow's date
 const tomorrow = new Date();
 tomorrow.setDate(new Date().getDate() + 1);
 
-describe('Activities API', () => {
+describe("Activities API", () => {
   beforeAll(() => {
     db.sync();
   });
 
-  describe('Create Activity Route', () => {
-    it('should create a new activity', async () => {
-      const res = await request(app).post('/api/events/1/activities').send({
-        activityName: 'Spin the bottle',
+  describe("Create Activity Route", () => {
+    afterEach(async () => {
+      const activity = Activity.destroy({
+        where: {
+          activityName: "Spin the bottle",
+        },
+      });
+    });
+    it("should create a new activity", async () => {
+      const res = await request(app).post("/api/events/1/activities").send({
+        activityName: "Spin the bottle",
         description:
           "Let's spin the bottle and whoever it points to has to debug code",
-        location: 'asylum',
+        location: "asylum",
         startDate: Date.now(),
         endDate: tomorrow,
-        startTime: '07:00 AM',
-        endTime: '05:00 PM',
+        startTime: "07:00 AM",
+        endTime: "05:00 PM",
         EventId: 1,
       });
       expect(res.status).toEqual(201);
     });
   });
 
-  describe('Get All Activities Route', () => {
-    it('should get all activities', async () => {
-      const res = await request(app).get('/api/events/1/activities');
+  describe("Get All Activities Route", () => {
+    it("should get all activities", async () => {
+      const res = await request(app).get("/api/events/1/activities");
       expect(res.status).toEqual(200);
     });
   });
 
-  describe('Get Single Activity Route', () => {
-    it('should get a single activity', async () => {
-      const res = await request(app).get('/api/events/1/activities/1');
+  describe("Get Single Activity Route", () => {
+    it("should get a single activity", async () => {
+      const res = await request(app).get("/api/events/1/activities/1");
       expect(res.status).toEqual(200);
     });
   });
 
-  describe('Delete Single Activity Route', () => {
-    it('should delete a single activity', async () => {
-      const res = await request(app).delete('/api/events/3/activities/5');
-      expect(res.status).toEqual(200);
-    });
-  });
-
-  describe('Update activity Route', () => {
-    it('should update a single activity', async () => {
-      const res = await request(app).put('/api/events/1/activities/1').send({
-        activityName: '3-Legged Race',
-        description: 'Get tied up to Charlie Brown and try to win the race',
-        location: 'Snoopy dog house',
+  describe("Delete Single Activity Route", () => {
+    beforeEach(async () => {
+      const testActivity = await Activity.create({
+        activityName: "Tacos Speed Eating Competition",
+        description: "See who can Beat Svetas record of 3 tacos per second",
+        location: "asylum",
         startDate: Date.now(),
         endDate: tomorrow,
-        startTime: '11:00 AM',
-        endTime: '10:00 PM',
-        EventId: 2,
+        startTime: "07:00 AM",
+        endTime: "05:00 PM",
+        EventId: 1,
       });
+    });
+    it("should delete a single activity", async () => {
+      const testActivity = await Activity.findOne({
+        where: { activityName: "Tacos Speed Eating Competition" },
+      });
+      const res = await request(app).delete(
+        `/api/events/${testActivity.EventId}/activities/${testActivity.id}`
+      );
+      expect(res.status).toEqual(200);
+    });
+  });
+
+  describe("Update activity Route", () => {
+    beforeEach(async () => {
+      const testActivity = await Activity.create({
+        activityName: "Tacos Speed Eating Competition",
+        description: "See who can Beat Svetas record of 3 tacos per second",
+        location: "asylum",
+        startDate: Date.now(),
+        endDate: tomorrow,
+        startTime: "07:00 AM",
+        endTime: "05:00 PM",
+        EventId: 1,
+      });
+    });
+
+    afterEach(async () => {
+      const testActivity = Activity.destroy({
+        where: {
+          activityName: "Tacos Speed Eating Competition",
+        },
+      });
+    });
+
+    it("should update a single activity", async () => {
+      const testActivity = await Activity.findOne({
+        where: { activityName: "Tacos Speed Eating Competition" },
+      });
+      const res = await request(app)
+        .put(
+          `/api/events/${testActivity.EventId}/activities/${testActivity.id}`
+        )
+        .send({
+          activityName: "3-Legged Race",
+          description: "Get tied up to Charlie Brown and try to win the race",
+          location: "Snoopy dog house",
+          startDate: Date.now(),
+          endDate: tomorrow,
+          startTime: "11:00 AM",
+          endTime: "10:00 PM",
+          EventId: 1,
+        });
       expect(res.status).toEqual(200);
     });
   });
