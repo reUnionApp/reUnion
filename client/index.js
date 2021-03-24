e31b5737252c83b586b4375991a36da2e305cf3f
Svetlana Leonova <leonova_svetlana@yahoo.com>
2021-03-24 18:06:06 -0400

    copied client folder files and auth-form component from graceshopper

---
+++ client/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { Provider } from "react-redux";
+import { Router } from "react-router-dom";
+import store from "./store";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <Router history={history}>
+        <App />
+      </Router>
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
