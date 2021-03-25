65da97cb72946d1b2a3be01c9785c911bc774083
Farhad Siraj <farhadsiraj@gmail.com>
2021-03-25 16:46:39 -0400

    feat(FE): installed axios and redux-devtools

--- src/store/index.js
+++ src/store/index.js
@@ -2,8 +2,9 @@ import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { createLogger } from 'redux-logger';
 import thunkMiddleware from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
+import authReducer from './authReducer';
 
-const reducer = combineReducers({});
+const reducer = combineReducers({ authReducer });
 
 const middleware = composeWithDevTools(
   applyMiddleware(thunkMiddleware, createLogger({ collapsed: true }))
