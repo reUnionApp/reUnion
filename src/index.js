e1dc1eb7aedb0a1978c0f3d2eebd609fdb872219
Svetlana Leonova <leonova_svetlana@yahoo.com>
2021-03-26 17:03:54 -0400

    feat(FE): added favicon

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import { Provider } from 'react-redux';
-import { Router } from 'react-router-dom';
-import store from './store';
-import history from './history';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { Provider } from "react-redux";
+import { Router } from "react-router-dom";
+import store from "./store";
+import history from "./history";
 
 ReactDOM.render(
   <React.StrictMode>
@@ -14,5 +14,5 @@ ReactDOM.render(
       </Router>
     </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
