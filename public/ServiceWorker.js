35af543fe333e566d64b4f3abb56f26c9949f8a0
Farhad Siraj <farhadsiraj@gmail.com>
2021-03-29 18:08:11 -0400

    fix(FE): fixed service worker

--- public/ServiceWorker.js
+++ public/ServiceWorker.js
@@ -1,16 +1,16 @@
-let CACHE_NAME = "my-site-cache-v1";
-const urlsToCache = ["/", "/index.html"];
-self.addEventListener("install", function (event) {
+let CACHE_NAME = 'my-site-cache-v1';
+const urlsToCache = ['/', '/index.html'];
+self.addEventListener('install', function (event) {
   // Perform install steps
   event.waitUntil(
     caches.open(CACHE_NAME).then(function (cache) {
-      console.log("Opened cache");
+      console.log('Opened cache');
       return cache.addAll(urlsToCache);
     })
   );
 });
 
-self.addEventListener("fetch", function (event) {
+self.addEventListener('fetch', function (event) {
   event.respondWith(
     caches.match(event.request).then(function (response) {
       if (response) {
@@ -21,11 +21,11 @@ self.addEventListener("fetch", function (event) {
   );
 });
 
-self.addEventListener("install", function (event) {
+self.addEventListener('install', function (event) {
   // Perform install steps
   event.waitUntil(
     caches.open(CACHE_NAME).then(function (cache) {
-      console.log("Opened cache");
+      console.log('Opened cache');
       return cache.addAll(urlsToCache);
     })
   );
