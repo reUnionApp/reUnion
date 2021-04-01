87d2cc1fb67262707988ba7595a0e9ac012ada74
Svetlana Leonova <leonova_svetlana@yahoo.com>
2021-04-01 17:27:44 -0400

    style(FE): signup form styled using materialUI

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,10 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { withRouter, Route, Switch } from 'react-router-dom';
-import PropTypes from 'prop-types';
-import { Login, Signup, Homepage, LandingPage } from './components';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { withRouter, Route, Switch } from "react-router-dom";
+import PropTypes from "prop-types";
+import { Login, Signup, Homepage, LandingPage } from "./components";
 
-import { me } from './store';
+import { me } from "./store";
 
 class Routes extends Component {
   componentDidMount() {
