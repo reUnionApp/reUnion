10dde0b053d9aebcd1eae80976bb5928d5f3aa35
Svetlana Leonova <leonova_svetlana@yahoo.com>
2021-04-16 16:38:43 -0400

    refactor(FE): rearranged styles folder structure

--- src/components/landingPage.js
+++ src/components/landingPage.js
@@ -1,18 +1,12 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import SwiperCore, { Navigation, Pagination, A11y } from 'swiper';
+import React from "react";
+import { Link } from "react-router-dom";
+import { Swiper, SwiperSlide } from "swiper/react";
+import SwiperCore, { Navigation, Pagination, A11y } from "swiper";
 
-import 'swiper/swiper.scss';
-import 'swiper/components/navigation/navigation.scss';
-import 'swiper/components/pagination/pagination.scss';
-// import '../styles/landingpagestyle.css';
-
-import plan from '../styles/plan.jpg';
-import invite from '../styles/invite.jpg';
-import reunite from '../styles/reunite.jpg';
-import logo from '../styles/reUnion_Logo.png';
-import title from '../styles/reUnion_Title.png';
+import "swiper/swiper.scss";
+import "swiper/components/navigation/navigation.scss";
+import "swiper/components/pagination/pagination.scss";
+import "../styles/landingPage.css";
 
 SwiperCore.use([Navigation, Pagination, A11y]);
 
@@ -28,26 +22,48 @@ const LandingPage = () => {
       >
         <SwiperSlide>
           <div className="image-container">
-            <img style={{ width: '50vw' }} src={title} alt="" id="title-img" />
-            <img style={{ width: '50vw' }} src={logo} alt="" id="logo-img" />
+            <img
+              style={{ width: "50vw", maxWidth: "300px" }}
+              src="reUnion_Title.png"
+              alt="reUnion title"
+              id="title-img"
+            />
+            <img
+              style={{ width: "50vw", maxWidth: "300px" }}
+              src="reUnion_Logo.png"
+              alt="reUnion logo"
+              id="logo-img"
+            />
           </div>
         </SwiperSlide>
         <SwiperSlide>
           <div className="image-container">
-            <img style={{ width: '90vw' }} src={plan} alt="plan image" />
             <p className="landingDesc">Plan</p>
+            <img
+              style={{ width: "90vw", maxWidth: "300px" }}
+              src="plan.jpg"
+              alt="plan image"
+            />
           </div>
         </SwiperSlide>
         <SwiperSlide>
           <div className="image-container">
-            <img style={{ width: '90vw' }} src={invite} alt="invite image" />
             <p className="landingDesc">Invite</p>
+            <img
+              style={{ width: "90vw", maxWidth: "300px" }}
+              src="invite.jpg"
+              alt="invite image"
+            />
           </div>
         </SwiperSlide>
         <SwiperSlide>
           <div className="image-container">
-            <img style={{ width: '90vw' }} src={reunite} alt="reunite image" />
-            <p className="landingDesc">Re-Unite</p>
+            <p className="landingDesc">re-Unite</p>
+            <img
+              style={{ width: "90vw", maxWidth: "300px" }}
+              src="reunite.jpg"
+              alt="reunite image"
+            />
           </div>
         </SwiperSlide>
       </Swiper>
