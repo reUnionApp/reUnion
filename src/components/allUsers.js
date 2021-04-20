fde63ee965f9b0c7f25905631f66a4f0dbf405f8
Jenny Zeng <jzeng7077@gmail.com>
2021-04-20 18:01:58 -0400

    Feat(FE): All users reducer & component added

---
+++ src/components/allUsers.js
@@ -0,0 +1,34 @@
+import React, { useEffect } from "react";
+import { connect } from "react-redux";
+import { getUsers } from "../store";
+
+function AllUsers(props) {
+  console.log("users", props);
+  useEffect(() => {
+    props.getUsers();
+  }, []);
+  return (
+    <div>
+      <h2>All Users:</h2>
+      {props.users && props.users.map((user) => {
+        return (
+          <div key={user.id}>
+            <ul>
+              <li>{user.firstName}</li>
+            </ul>
+          </div>
+        )
+      })}
+    </div>
+  );
+}
+
+const mapState = (state) => ({
+  users: state.allUsersReducer,
+});
+
+const mapDispatch = (dispatch) => ({
+  getUsers: () => dispatch(getUsers()),
+});
+
+export default connect(mapState, mapDispatch)(AllUsers);
