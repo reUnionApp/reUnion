1c41d8bde11fe1a2ab060fac48abff1e83ebb332
Svetlana Leonova <leonova_svetlana@yahoo.com>
2021-04-22 18:11:57 -0400

    feat(FE): update profile form working

--- src/components/profile.js
+++ src/components/profile.js
@@ -1,23 +1,27 @@
-import React, { useState, useEffect } from 'react';
-import { connect } from 'react-redux';
-import { getUser, deleteUser } from '../store';
-import { Link } from 'react-router-dom';
-import history from '../history';
-import '../styles/profile.css';
+import React, { useState, useEffect } from "react";
+import { connect } from "react-redux";
+import { getUser, deleteUser } from "../store";
+import { Link } from "react-router-dom";
+import history from "../history";
+import "../styles/profile.css";
 
-const profile = (props) => {
+const Profile = (props) => {
   const goToUpdateProfile = () => {
-    props.history.push('/updateprofile');
+    props.history.push("/updateprofile");
   };
+  // props.getUser(props.login.id);
+  useEffect(() => {
+    props.getUser(props.user.id);
+  }, []);
   return (
     <>
       <hr />
       <div className="w100 flex jContentSB aItemsC">
         <h1
-          style={{ marginLeft: '15px' }}
+          style={{ marginLeft: "15px" }}
         >{`${props.user.firstName}'s Profile`}</h1>
         <button
-          style={{ marginRight: '15px' }}
+          style={{ marginRight: "15px" }}
           type="button"
           className="button updateProfile yellow"
           onClick={goToUpdateProfile}
@@ -25,12 +29,12 @@ const profile = (props) => {
           Update Profile
         </button>
       </div>
-      <div style={{ margin: '36px 0px 36px 36px' }}>
+      <div style={{ margin: "36px 0px 36px 36px" }}>
         <h4>
           Email: <span className="light">{props.user.email}</span>
         </h4>
         <h4>
-          Dietary Restrictions:{' '}
+          Dietary Restrictions:{" "}
           {props.user.dietaryRestrictions ? (
             <span className="light">{props.user.dietaryRestrictions}</span>
           ) : (
@@ -38,7 +42,7 @@ const profile = (props) => {
           )}
         </h4>
         <h4>
-          Special Requests:{' '}
+          Special Requests:{" "}
           {props.user.specialRequests ? (
             <span className="light">{props.user.specialRequests}</span>
           ) : (
@@ -51,12 +55,14 @@ const profile = (props) => {
 };
 
 const mapState = (state) => ({
-  user: state.authReducer,
+  login: state.authReducer,
+  user: state.userReducer,
   userEvents: state.allEventsReducer.userEvents,
 });
 
 const mapDispatch = (dispatch) => ({
+  getUser: (userId) => dispatch(getUser(userId)),
   deleteUser: (userId) => dispatch(deleteUser(userId)),
 });
 
-export default connect(mapState, mapDispatch)(profile);
+export default connect(mapState, mapDispatch)(Profile);
