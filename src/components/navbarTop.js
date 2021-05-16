bd3c924c8df924eabb542928aa0b9d5bfca6b972
Derek Louis <thedereklouis@gmail.com>
2021-05-16 14:07:30 -0400

    added nav bar logo

--- src/components/navbarTop.js
+++ src/components/navbarTop.js
@@ -8,33 +8,38 @@ import '../styles/navbarTop.css';
 export const toggleBurger = () => {
   const burger = document.getElementById('circleBurger');
   const filler = document.getElementById('circleFiller');
+  const navLogo = document.getElementById('navLogo');
   const plusButton = document.getElementById('plusButton');
   const darken = document.getElementById('darken');
   if (filler.className === 'cfClose') {
     filler.className = 'cfOpen';
     burger.className = 'cbOpen';
+    navLogo.className = 'navLogoFade';
     plusButton.className = 'plusButtonOff';
     darken.className = 'darkenOn';
   } else {
     filler.className = 'cfClose';
     burger.className = 'cbClose';
+    navLogo.className = 'navLogoFull';
     plusButton.className = 'plusButtonOn';
     darken.className = 'darkenOff';
   }
 };
 
 const plusFlash = () => {
-  const plusButton = document.getElementById('plusButton');
-  plusButton.className = 'plusButtonOff';
-  let count = 0;
-  plusButton.addEventListener('transitionend', function (e) {
-    if (e.target.className === 'plusButtonOff') {
-      count++;
-    }
-    if (count === 2) {
-      plusButton.className = 'plusButtonOn';
-    }
-  });
+  if (window.location.pathname !== '/createEvent') {
+    const plusButton = document.getElementById('plusButton');
+    plusButton.className = 'plusButtonOff';
+    let count = 0;
+    plusButton.addEventListener('transitionend', function (e) {
+      if (e.target.className === 'plusButtonOff') {
+        count++;
+      }
+      if (count === 2) {
+        plusButton.className = 'plusButtonOn';
+      }
+    });
+  }
 };
 
 const NavbarTop = () => {
@@ -63,6 +68,12 @@ const NavbarTop = () => {
           <div id="circleFiller" className="cfClose"></div>
         </div>
       </div>
+      <img
+        src="reUnion_Title.png"
+        alt="logo"
+        id="navLogo"
+        className="navLogoFull"
+      />
       <Link to="/createEvent" id="plusLink" onClick={plusFlash}>
         <div id="plusButtonContainer">
           <div id="plusButton" className="plusButtonOn"></div>
