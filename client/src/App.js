342f988e1ed82c3f700bc02bcdcecc07fd6e7613
Derek Louis <thedereklouis@gmail.com>
2021-06-22 19:46:25 -0400

    FEAT(FE) Added responsive design and cleaned up components for animation, navbar top, drop menu, and landing page

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,22 @@ import Routes from './routes';
 import { Navbar } from './components';
 import './styles/app.css';
 
+let isMobile = false;
+
+if (
+  /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+    navigator.userAgent
+  )
+) {
+  isMobile = true;
+}
+
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   return (
-    <div className="App">
-      <Routes />
+    <div className={`App ${isMobile ? 'mobile' : 'desktop'}`}>
+      <Routes isMobile={isMobile} />
     </div>
   );
 }
--- client/src/components/landingPage.js
+++ client/src/components/landingPage.js
@@ -10,91 +10,67 @@ import '../styles/landingPage.css';
 
 SwiperCore.use([Navigation, Pagination, A11y]);
 
-const LandingPage = () => {
+const LandingPage = (props) => {
   return (
     <div
-      className="flex column jContentC aItemsC"
-      style={{
-        height: '100vh',
-      }}
+      id="LPMaster"
+      className={`flex column jContentC aItemsC ${
+        props.isMobile ? 'mobile' : 'desktop'
+      }`}
     >
-      <div>
-        <Swiper
-          className="landing-wrapper"
-          effect="fade"
-          spaceBetween={0}
-          slidesPerView={1}
-          navigation
-          pagination={{ clickable: true, dynamicBullets: true }}
-        >
-          <SwiperSlide>
-            <div className="image-container">
-              <img
-                style={{ maxWidth: '350px' }}
-                src="reUnion_Title.png"
-                alt="reUnion title"
-                id="title-img"
-              />
-              <img
-                style={{ maxWidth: '200px', margin: '100px 0px 0px 0px' }}
-                src="reUnion_Logo.png"
-                alt="reUnion logo"
-                id="logo-img"
-              />
-            </div>
-          </SwiperSlide>
-          <SwiperSlide>
-            <p style={{ color: '#e40065' }} className="titleClass openSans ">
-              Plan
-            </p>
-            <div className="image-container">
-              <img
-                style={{ maxWidth: '250px' }}
-                src="plan.jpg"
-                alt="plan art"
-              />
-            </div>
-          </SwiperSlide>
-          <SwiperSlide>
-            <p style={{ color: '#e40065' }} className="titleClass openSans ">
-              Invite
-            </p>
-            <div className="image-container">
-              <img
-                style={{ maxWidth: '250px' }}
-                src="invite.jpg"
-                alt="invite art"
-              />
-            </div>
-          </SwiperSlide>
-          <SwiperSlide>
-            <p style={{ color: '#e40065' }} className="titleClass openSans ">
-              re-Unite
-            </p>
-            <div className="image-container">
-              <img
-                style={{ maxWidth: '350px' }}
-                src="reunite.jpg"
-                alt="reunite art"
-              />
-            </div>
-          </SwiperSlide>
-        </Swiper>
-        <div className="button-container flex jContentC ">
-          <button className="button pink-black" id="landing-page-btn">
-            <Link
-              style={{
-                textDecoration: 'none',
-                color: 'white',
-                fontSize: '18px',
-              }}
-              to="/login"
-              className="get-started"
-            >
-              Get Started
-            </Link>
-          </button>
-        </div>
+      <Swiper
+        className="landing-wrapper"
+        effect="fade"
+        spaceBetween={0}
+        slidesPerView={1}
+        navigation
+        pagination={{ clickable: true, dynamicBullets: true }}
+      >
+        <SwiperSlide>
+          <div className="image-container">
+            <img src="reUnion_Title.png" alt="reUnion title" id="LPTitle-Img" />
+            <img src="reUnion_Logo.png" alt="reUnion logo" id="LPLogo-Img" />
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <p style={{ color: '#e40065' }} className="titleClass openSans ">
+            Plan
+          </p>
+          <div className="image-container">
+            <img id="LPPlanImg" src="plan.jpg" alt="plan art" />
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <p style={{ color: '#e40065' }} className="titleClass openSans ">
+            Invite
+          </p>
+          <div className="image-container">
+            <img id="LPInviteImg" src="invite.jpg" alt="invite art" />
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <p style={{ color: '#e40065' }} className="titleClass openSans ">
+            re-Unite
+          </p>
+          <div className="image-container">
+            <img id="LPReuniteImg" src="reunite.jpg" alt="reunite art" />
+          </div>
+        </SwiperSlide>
+      </Swiper>
+      <div className="button-container flex jContentC ">
+        <button className="button pink-black" id="landing-page-btn">
+          <Link
+            style={{
+              textDecoration: 'none',
+              color: 'white',
+              fontSize: '18px',
+            }}
+            to="/login"
+            className="get-started"
+          >
+            Get Started
+          </Link>
+        </button>
       </div>
     </div>
   );
